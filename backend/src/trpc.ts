9ed0f6baeee7c0a946b27adc53f74ec30603bca5
Konstantin Konev <kost_87@list.ru>
2025-03-22 19:37:08 +0000

    Added backend

---
+++ backend/src/trpc.ts
@@ -0,0 +1,17 @@
+import { initTRPC } from '@trpc/server'
+
+const posts = [
+  { id: 1, name: 'Post 1', description: 'Post 1 description...' },
+  { id: 2, name: 'Post 2', description: 'Post 2 description...' },
+  { id: 3, name: 'Post 3', description: 'Post 3 description...' },
+  { id: 4, name: 'Post 4', description: 'Post 4 description...' },
+  { id: 5, name: 'Post 5', description: 'Post 5 description...' },
+]
+
+const trpc = initTRPC.create()
+
+export const trpcRouter = trpc.router({
+  getPosts: trpc.procedure.query(() => {
+    return { posts }
+  }),
+})
